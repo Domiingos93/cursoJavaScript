4c11e1e253f1a7c5f9ac14d25264e078801cdc8d
Domiingos93 <glauco.domingues@hotmail.com>
2020-01-13 15:17:44 -0300

    all

---
+++ programacao-sincrona-e-assincrona/js/js-assincrono.js
@@ -0,0 +1,8 @@
+console.log("inicio programação síncrona.");
+
+//Função timer programação assíncrona
+setTimeout(function(){
+    console.log("Executando programação assíncrona");
+}, 2000);
+
+console.log("termino programação síncrona.");
